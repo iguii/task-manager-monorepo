6a810d55e1bdbb6b86dab6549dc4fdb34754e4f4
iguii <igui10082002@gmail.com>
2026-04-18 14:42:18 -0400

    Add Task Controller CRUD

---
+++ src/controllers/task.controller.ts
@@ -0,0 +1,95 @@
+import type {Request, Response} from "express";
+import {prisma} from "../lib/prisma";
+import e from "cors";
+
+export const createTask = async(req: Request, res: Response) => {
+    let statusCode = 201;
+    let newTask;
+
+    try {
+        newTask = await prisma.task.create({
+            data: {
+                ...req.body,
+            }
+        });
+    }catch (err: any) {
+        statusCode = 500;
+        newTask = {
+            message: `${err.message}`,
+        }
+    }
+
+    return res.status(statusCode).json(newTask);
+}
+
+export const readTasks = async (req: Request, res: Response) => {
+    const tasks = await prisma.task.findMany();
+
+    return res.status(200).json(tasks);
+}
+
+export const updateTask = async (req: Request, res: Response) => {
+    const id = parseInt(<string>req.params.taskId);
+    let statusCode = 200;
+    let updatedTask;
+
+    try{
+        const found = await prisma.task.findUnique({ where: { id: id } });
+        if(!found) {
+            statusCode = 404;
+            updatedTask = { message: `Task not found` };
+        } else updatedTask = await prisma.task.update({
+            where: { id: id },
+            data: {
+                id: id,
+                ...req.body,
+            }
+        })
+    }catch (e: any) {
+        statusCode = 500;
+        updatedTask = { message: `${e.message}` };
+
+    }
+
+    return res.status(statusCode).json(updatedTask);
+}
+
+export const deleteTask = async (req: Request, res: Response) => {
+    const id = parseInt(<string>req.params.id);
+    let statusCode = 200;
+    let task;
+
+    try{
+        task = await prisma.task.findUnique({ where: { id: id} });
+        if (!task) {
+            statusCode = 404;
+        } else await prisma.task.delete({ where: { id: id } });
+    } catch (e: any) {
+        statusCode = 500;
+        task = { message: `${e.message}` };
+    }
+
+    return res.status(statusCode).json(task);
+}
+
+
+export const toggleTask = async (req: Request, res: Response) => {
+    const id = parseInt(<string>req.params.id);
+    let statusCode = 200;
+    let task;
+
+    try{
+        task = await prisma.task.findUnique({ where: { id: id } });
+        if (!task) {
+            statusCode = 404;
+            task = { message: "Task not found" };
+        } else task = await prisma.task.update({
+                where: { id: id },
+                data: { completed: !task.completed },
+        });
+    } catch (e: any) {
+        statusCode = 500;
+        task = { message: `${e.message}` };
+    }
+    return res.status(statusCode).json(task);
+}
