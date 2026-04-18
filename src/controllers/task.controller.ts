7916b4344c1c91cfc17214ca5b9ed19b90fc659f
iguii <igui10082002@gmail.com>
2026-04-18 17:08:52 -0400

    Implement tag assignment

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,14 +1,40 @@
 import type {Request, Response} from "express";
 import {prisma} from "../lib/prisma";
+
+//TODO: modify logic to add tags to tasks
 export const createTask = async(req: Request, res: Response) => {
     let statusCode = 201;
     let newTask;
 
     try {
+        const { title, description, tags = [] } = req.body;
+
+        // clean tags
+        const cleanedTags = [...new Set(
+            tags.map((tag:any) => tag.trim().toLowerCase())
+                .filter((tag:any) => tag.length > 0)
+        )]
+
+        // create all non-existing tags
+        for (const tagName of cleanedTags) {
+            await prisma.tag.upsert({
+               where: { name: <string>tagName },
+               update: {},
+               create: { name: <string>tagName },
+           })
+        }
+
         newTask = await prisma.task.create({
             data: {
-                ...req.body,
-            }
+                title: title.trim(),
+                description: description.trim(),
+                tags: {
+                    connect: cleanedTags.map((tagName: any) => ({name: tagName})),
+                }
+            },
+            include: {
+                tags: true,
+            },
         });
     }catch (err: any) {
         statusCode = 500;
@@ -21,7 +47,11 @@ export const createTask = async(req: Request, res: Response) => {
 }
 
 export const readTasks = async (req: Request, res: Response) => {
-    const tasks = await prisma.task.findMany();
+    const tasks = await prisma.task.findMany({
+        include: {
+            tags: true,
+        }
+    });
 
     return res.status(200).json(tasks);
 }
@@ -32,21 +62,46 @@ export const updateTask = async (req: Request, res: Response) => {
     let updatedTask;
 
     try{
+        const {tags = [], ...rest} = req.body;
+
         const found = await prisma.task.findUnique({ where: { id: id } });
+
         if(!found) {
             statusCode = 404;
             updatedTask = { message: `Task not found` };
-        } else updatedTask = await prisma.task.update({
-            where: { id: id },
-            data: {
-                id: id,
-                ...req.body,
+        } else {
+            // clean tags
+            const cleanedTags = [...new Set(
+                tags.map((tag: string) => tag.trim().toLowerCase())
+                    .filter((tag: any) => tag.length > 0)
+            )];
+
+
+            // create all non-existing tags
+            for (const tagName of cleanedTags) {
+                await prisma.tag.upsert({
+                    where: {name: <string>tagName},
+                    update: {},
+                    create: {name: <string>tagName},
+                })
             }
-        })
+
+            updatedTask = await prisma.task.update({
+                where: {id: id},
+                data: {
+                    ...rest,
+                    tags: {
+                        set: cleanedTags.map((name: any) => ({name}))
+                    },
+                },
+                include: {
+                    tags: true,
+                }
+            });
+        }
     }catch (e: any) {
         statusCode = 500;
         updatedTask = { message: `${e.message}` };
-
     }
 
     return res.status(statusCode).json(updatedTask);
