d9336b16bc7d72a0717ec572741b3aade4d6d2ea
iguii <igui10082002@gmail.com>
2026-07-14 15:42:04 -0400

    Code: Add vitest/coverage-v8 & config

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -11,5 +11,16 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/test/setup.js",
+    exclude: ["e2e/**", 'node_modules/**', 'dist/**'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      thresholds: {
+        lines: 60,
+        functions: 60,
+        branches: 50,
+        statements: 60,
+      },
+    },
   },
 });
