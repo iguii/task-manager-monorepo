b8de24590712490d3d8bbe5d139e2b0b6b04dbbb
iguii <igui10082002@gmail.com>
2026-07-22 19:49:33 -0400

    Code: fix compiler options for build

--- backend/src/controllers/tag.controller.ts
+++ backend/src/controllers/tag.controller.ts
@@ -1,70 +1,70 @@
-import type {Request, Response} from "express";
-import {prisma} from "../lib/prisma";
+import type { Request, Response } from "express";
+import { prisma } from "../lib/prisma.js";
 
 export const createTag = async (req: Request, res: Response) => {
-    let statusCode = 201;
-    let newTag;
+  let statusCode = 201;
+  let newTag;
 
-    try {
-        newTag = await prisma.tag.create({
-            data: {
-                name: req.body.name,
-            }
-        })
-    } catch (error: any) {
-       statusCode = 500;
-       newTag = { message: error.message };
-    }
+  try {
+    newTag = await prisma.tag.create({
+      data: {
+        name: req.body.name,
+      }
+    })
+  } catch (error: any) {
+    statusCode = 500;
+    newTag = { message: error.message };
+  }
 
-    return res.status(statusCode).json(newTag);
+  return res.status(statusCode).json(newTag);
 }
 
 export const readTags = async (req: Request, res: Response) => {
-    const statusCode = 200;
-    const tags = await prisma.tag.findMany();
+  const statusCode = 200;
+  const tags = await prisma.tag.findMany();
 
-    return res.status(statusCode).json(tags);
+  return res.status(statusCode).json(tags);
 }
 
 export const updateTag = async (req: Request, res: Response) => {
-    let statusCode = 200;
-    const id = parseInt(<string>req.params.id);
-    let tag
+  let statusCode = 200;
+  const id = parseInt(<string>req.params.id);
+  let tag
 
-    try {
-        tag = await prisma.tag.findUnique({ where: { id: id } });
-        if (!tag) {
-            statusCode = 404;
-            tag = { message: "Tag not found" };
-        } else tag = await prisma.tag.update({
-            where: { id: id },
-            data: {
-                id: id,
-                ...req.body,
-            }
-        })
-    } catch (error: any) {
-       statusCode = 500;
-       tag = { message: error.message };
-    }
-    return res.status(statusCode).json(tag);
+  try {
+    tag = await prisma.tag.findUnique({ where: { id: id } });
+    if (!tag) {
+      statusCode = 404;
+      tag = { message: "Tag not found" };
+    } else tag = await prisma.tag.update({
+      where: { id: id },
+      data: {
+        id: id,
+        ...req.body,
+      }
+    })
+  } catch (error: any) {
+    statusCode = 500;
+    tag = { message: error.message };
+  }
+  return res.status(statusCode).json(tag);
 }
 
 export const deleteTag = async (req: Request, res: Response) => {
-    let statusCode = 200;
-    const id = parseInt(<string>req.params.id);
-    let tag;
+  let statusCode = 200;
+  const id = parseInt(<string>req.params.id);
+  let tag;
 
-    try {
-        tag = await prisma.tag.findUnique({ where: { id: id } });
-        if (!tag) {
-            statusCode = 404;
-            tag = { message: "Tag not found" };
-        }else await prisma.tag.delete({ where: { id: id } });
-    } catch (error: any) {
-        statusCode = 500;
-        tag = { message: error.message };
-    }
+  try {
+    tag = await prisma.tag.findUnique({ where: { id: id } });
+    if (!tag) {
+      statusCode = 404;
+      tag = { message: "Tag not found" };
+    } else await prisma.tag.delete({ where: { id: id } });
+  } catch (error: any) {
+    statusCode = 500;
+    tag = { message: error.message };
+  }
 
-    return res.status(statusCode).json(tag);
+  return res.status(statusCode).json(tag);
 }
--- backend/src/routes/tag.routes.ts
+++ backend/src/routes/tag.routes.ts
@@ -1,5 +1,5 @@
-import {Router} from "express";
-import {createTag, deleteTag, readTags, updateTag} from "../controllers/tag.controller";
+import { Router } from "express";
+import { createTag, deleteTag, readTags, updateTag } from "../controllers/tag.controller.js";
 
 const router = Router();
 
